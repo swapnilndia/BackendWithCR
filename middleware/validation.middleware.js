70437fbc0456479df0ffe7d0161280b475a93bf6
CKsisodia <cksisodia3@gmail.com>
2024-03-30 23:28:18 +0530

    Expense tracker all CRUD operation with validation

--- middleware/validation.middleware.js
+++ middleware/validation.middleware.js
@@ -1,30 +1,73 @@
-const ApiError = require('../utils/ApiError')
-const { signInSchema, todoValidationSchema } = require('../utils/validationSchema')
+const ApiError = require("../utils/ApiError");
+const {
+  signInSchema,
+  todoValidationSchema,
+  expenseValidationSchema,
+} = require("../utils/validationSchema");
 
 const signInValidation = async (req, res, next) => {
-    try {
-        await signInSchema.validate(req.body, { abortEarly: false })
-        next()
-    } catch (error) {
-        const errors = error.inner.map(err => ({
-            field: err.path,
-            message: err.message
-        }));
-        return res.status(400).json(new ApiError(400, 'VALIDATION ERROR', 'one or more validation error', errors));
-    }
-}
+  try {
+    await signInSchema.validate(req.body, { abortEarly: false });
+    next();
+  } catch (error) {
+    const errors = error.inner.map((err) => ({
+      field: err.path,
+      message: err.message,
+    }));
+    return res
+      .status(400)
+      .json(
+        new ApiError(
+          400,
+          "VALIDATION ERROR",
+          "one or more validation error",
+          errors
+        )
+      );
+  }
+};
 
 const todoValidation = async (req, res, next) => {
-    try {
-        await todoValidationSchema.validate(req.body, { abortEarly: false })
-        next()
-    } catch (error) {
-        const errors = error.inner.map(err => ({
-            field: err.path,
-            message: err.message
-        }))
-        return res.status(400).json(new ApiError(400, 'VALIDATION ERROR', 'one or more validation error', errors));
-    }
-}
+  try {
+    await todoValidationSchema.validate(req.body, { abortEarly: false });
+    next();
+  } catch (error) {
+    const errors = error.inner.map((err) => ({
+      field: err.path,
+      message: err.message,
+    }));
+    return res
+      .status(400)
+      .json(
+        new ApiError(
+          400,
+          "VALIDATION ERROR",
+          "one or more validation error",
+          errors
+        )
+      );
+  }
+};
+const expenseTrackerValidation = async (req, res, next) => {
+  try {
+    await expenseValidationSchema.validate(req.body, { abortEarly: false });
+    next();
+  } catch (error) {
+    const errors = error.inner.map((err) => ({
+      field: err.path,
+      message: err.message,
+    }));
+    return res
+      .status(400)
+      .json(
+        new ApiError(
+          400,
+          "VALIDATION ERROR",
+          "one or more validation error",
+          errors
+        )
+      );
+  }
+};
 
-module.exports = { signInValidation ,todoValidation}
+module.exports = { signInValidation, todoValidation, expenseTrackerValidation };
--- utils/validationSchema.js
+++ utils/validationSchema.js
@@ -1,43 +1,68 @@
 // validationSchemas.js
-const yup = require('yup');
+const yup = require("yup");
 
 const signUpSchema = yup.object().shape({
-    username: yup.string()
-        .required()
-        .min(8, 'Username must name by 8 characters long')
-        .matches(/^(?=.*[a-zA-Z])(?=.*\d).+$/, 'Username must contain at least one alphabet and one number'),
-    email: yup.string()
-        .email()
-        .required(),
-    password: yup.string()
-        .required()
-        .min(8, 'Password must name by 8 characters long')
-        .matches(
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-            'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character'
-        ),
+  username: yup
+    .string()
+    .required()
+    .min(8, "Username must name by 8 characters long")
+    .matches(
+      /^(?=.*[a-zA-Z])(?=.*\d).+$/,
+      "Username must contain at least one alphabet and one number"
+    ),
+  email: yup.string().email().required(),
+  password: yup
+    .string()
+    .required()
+    .min(8, "Password must name by 8 characters long")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    ),
 });
 const signInSchema = yup.object().shape({
-    username: yup.string()
-        .required(),
-    password: yup.string()
-        .required()
-
+  username: yup.string().required(),
+  password: yup.string().required(),
 });
 
 const todoValidationSchema = yup.object().shape({
-    title: yup.string()
-        .required(),
-    description: yup.string()
-        .required(),
-    completed: yup.boolean(),
-    dueDate: yup.date()
-        .required().max(new Date(new Date().getTime() + (30 * 24 * 60 * 60 * 1000)), "Due date must be within the next thirty days"),
-    todoCategory: yup.string()
-        .required(),
-    priority: yup.string().oneOf(['HIGH', 'MEDIUM', 'LOW'], "Invalid priority (Priority can only be one of 'HIGH', 'MEDIUM' or 'LOW')").default('LOW')
-})
+  title: yup.string().required(),
+  description: yup.string().required(),
+  completed: yup.boolean(),
+  dueDate: yup
+    .date()
+    .required()
+    .max(
+      new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000),
+      "Due date must be within the next thirty days"
+    ),
+  todoCategory: yup.string().required(),
+  priority: yup
+    .string()
+    .oneOf(
+      ["HIGH", "MEDIUM", "LOW"],
+      "Invalid priority (Priority can only be one of 'HIGH', 'MEDIUM' or 'LOW')"
+    )
+    .default("LOW"),
+});
+
+const expenseValidationSchema = yup.object().shape({
+  expenseID: yup.string().required(),
+  description: yup.string().required(),
+  amount: yup.number().required(),
+  date: yup
+    .date()
+    .required()
+    .max(
+      new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000),
+      "Due date must be within the next thirty days"
+    ),
+  location: yup.string().required(),
+});
 
 module.exports = {
-    signUpSchema, signInSchema, todoValidationSchema
+  signUpSchema,
+  signInSchema,
+  todoValidationSchema,
+  expenseValidationSchema,
 };
