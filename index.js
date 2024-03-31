03b67ad85e64ac1fe39e94f521939c0921afead0
swapnilndia <swapnil240695@gmail.com>
2024-03-31 12:54:24 +0530

    new message

--- index.js
+++ index.js
@@ -3,32 +3,26 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 require('dotenv').config()
 
-
 const todoRouter = require('./route/todo.route')
 const userRouter = require('./route/user.route')
 const expenseRouter = require('./route/expense.route')
 const studentRouter = require('./route/studentData.route')
 
-
 const app = express()
-
 
 app.use(bodyParser.json())
 app.use(cors())
 
-
-const PORT = (process.env.PORT)
+const PORT = process.env.PORT || 3000 // Setting a default port if PORT environment variable is not set
 
 app.use('/todo', todoRouter)
 app.use('/user', userRouter)
-app.use('/expense',expenseRouter )
-app.use('/studentdata',studentRouter)
+app.use('/expense', expenseRouter)
+app.use('/studentdata', studentRouter)
 
-app.get('/', async(req, res) => {
-    res.send(<h1>Hi Guys, </h1>)
+app.get('/', async (req, res) => {
+    res.send('<h1>Hi Guys,</h1>')
 })
-
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
