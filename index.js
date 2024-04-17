ba0d6ac0d9557ada32396c6cff6aeb5859e35a18
swapnilndia <swapnil240695@gmail.com>
2024-04-18 00:22:24 +0530

    new commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
 }));
 
 const PORT = process.env.PORT || 3000 // Setting a default port if PORT environment variable is not set
