cfacf7b6820cd1f6e3c5c65ca1db0cf2388ca789
swapnilndia <swapnil240695@gmail.com>
2024-04-18 00:30:39 +0530

    new try

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const studentRouter = require('./route/studentData.route')
 const app = express()
 
 app.use(bodyParser.json())
-app.use(cors(corsOptions));
+app.use(cors());
 
 const PORT = process.env.PORT || 3000 // Setting a default port if PORT environment variable is not set
 
